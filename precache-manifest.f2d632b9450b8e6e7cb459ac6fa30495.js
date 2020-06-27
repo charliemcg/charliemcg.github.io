ecdf0f12f360e9cd5e2a94470884b800d24da1cf
Charlie McGregor <charlie_mcgregor@outlook.com>
2020-06-27 18:22:06 +1000

    Attempting to upload react project to github pages

---
+++ precache-manifest.f2d632b9450b8e6e7cb459ac6fa30495.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "509b93ebd6a03dfa6f8ed2225e833459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe6854e7a12bdd307d6e",
+    "url": "/static/css/main.3562c07c.chunk.css"
+  },
+  {
+    "revision": "458472af74584f1edd67",
+    "url": "/static/js/2.d7436628.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d7436628.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe6854e7a12bdd307d6e",
+    "url": "/static/js/main.99793bb5.chunk.js"
+  },
+  {
+    "revision": "824a87c525fc41ffa25d",
+    "url": "/static/js/runtime-main.078ffda6.js"
+  },
+  {
+    "revision": "ba567b2a31fa1ce477e7b678190ea276",
+    "url": "/static/media/ReactLogo.ba567b2a.png"
+  }
+]);
